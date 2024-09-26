28b2ed7f50ed5b46caa9c61bdae388d7671d9d74
Alexis <210013@utxicotepec.edu.mx>
2024-09-26 11:13:06 -0600

    Practica 8
    ARCHIVOS PRACTICA 08

--- DARTPAD/Ex06_Funciones.dart
+++ DARTPAD/Ex06_Funciones.dart
@@ -1,13 +1,13 @@
 void main() {
-  final double totalCarList;
 
+  final double totalCalList;
   //1. Llamado de una funcion sin parametros
   greetEveryone();
-  
+
   //2. LLAMADO DE UNA FUNCION QUE RETORNA VALORES
   //A)  SIN ALMACENAR EL VALOR DE UNA VARIABLE
   print("hoy es el dia numero: ${getDayNumber()} del mes.");
-  //B) ALAMACENADO EL VALOR DE LA FUNCION EN UNA NUEVA VARIABLE
+  //B) ALAMACENADO EL VALOR DE LA FUNCION EN UNA NAUEVA VARIABLE
   final int diaMes;
   diaMes = getDayNumber();
   print("Entre tres dias sera: ${diaMes+3}");
@@ -36,29 +36,39 @@ print(greetSomeoneTyped(true));
  */
   //4. Llamado de una funcion con parametros opcionales
   //4.1 ENCVIANDO AMBOS PARAMETROS
-  print(greetHourOfDay("aLEXIS",9));
+  print(greetHourOfDay("Alexius",9));
   //4.2 SOLO ENVIADO EL OBLIGATORIO
-  print(greetHourOfDay("Alexius", null));
-  // 5. FUNCIONES LAMBDA - LAS FUNCIONES LAMBDA ES MEJOR CONOCIDAS COMO FUNCIONES ANONIMAS O FUNCIONES SIMPLIFICADAS SE EJECUTAN DE MANERA SIMPLE Y NO FRECUENTE EN LA EJECUCION DE UN PROGRMA O SISTEMA
-  var calculaCosto = (double productQuantity, double productPrice,
-  double percentageDiscount ) => (productQuantity * productPrice)*((100-percentageDiscount)/100);
+  print(greetHourOfDay("Alexis", null));
+  print(greetHourOfDay("Alexandra"));
+
+  //5. FUNCIONES LAMBA - LAS FUNCIONES LAMBA, MEJOR CONOCIDAS COMO FUNCIONES ANONIMAS O FUNCIONES SIMPLEFICADAS, SE EJECUTAN DE MANERA SIMPLE Y NO FRECUENTE EN LA EJECUCUION DE UN PROGRAMA O UN SISTEMA
+
+  var calculaCosto=(double productQuanbtity, double doubleProductPrice, double percentageDisscount)=>(productQuanbtity * doubleProductPrice)*((100-percentageDisscount)/100);
   double cantidadProducto = 5;
   double precioProducto = 125.50;
   double descuento = 2.5;
   print("""
-  Costo del producto: ${cantidadProducto};
-  Cantidad: ${cantidadProducto};
-  Descuento: ${descuento};
-  ------------------------------------------------
-  Costo del carrito de Compras: ${calculaCosto(cantidadProducto,
-  precioProducto, descuento)}
+  Costo del producto: ${precioProducto}
+  Cantidad: ${cantidadProducto}
+  Descuento: ${descuento}
+  ----------------------------------------------------------------
+  Costo del carrito de compras: ${calculaCosto(cantidadProducto, precioProducto, descuento)}
   """);
+//6. llamado de la funcion con parametros nombrados
+  print(infoCarListStatus(buyerName: "Alexandra"));
+
+  //6.1 llamado de una funcion con los parametros en desorden posicional
+  print(infoCarListStatus(
+    status: "En espera de pago",
+    amountCarList: 2416.20,
+    buyerName: "Juana"
+  ));
 }
 
 //FUNCIONES Y PARAMETROS 
-  
+
   //DECLARACION
-  
+
   //1. FUNCION SIN PARAMETROS
 void greetEveryone()
 
@@ -85,22 +95,33 @@ String greetSomeoneTyped(String personName)
 }
 
 // 4. FUNCIÓN CON PARÁMETROS OPCIONALES
-String greetHourOfDay(String personName, int? hora) {
-  hora ??= DateTime.now().hour; // Asignar la hora actual si hora es nulo
-  print("Hora:${hora}");
+String greetHourOfDay(String personName, [int? hourDay]) //Puede ser opcional
+{
+  hourDay ??= DateTime.now().hour; // si es nulo entonces ..
+  print("Hora: ${hourDay}");
   String saludo;
 
-  if (hora >= 6 && hora < 12) {
+  if (hourDay >= 6 && hourDay < 12) {
     saludo = "Buenos Días";
-  } else if (hora >= 12 && hora < 18) {
+  } else if (hourDay >= 12 && hourDay < 18) {
     saludo = "Buenas Tardes";
   } else {
     saludo = "Buenas Noches";
   }
 
   return "$saludo, $personName";
-  
+
+
 }
+String infoCarListStatus({
+  required String buyerName,
+  double amountCarList = 0.0,
+  String status = "Selección de productos",
+}) {
+  return "El carrito de compras de: $buyerName, tiene un total de: "
+      "\$${amountCarList} y actualmente está en estatus: $status";
+}
+
 
 
 
